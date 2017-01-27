aaeb9196e6b9d1ace10cd2ef124dfe286c9c5af2
Сергей <bugsmafia@gmail.com>
2017-01-27 15:49:51 +0500

    1

--- js/script.js
+++ js/script.js
@@ -555,6 +555,22 @@ document.addEventListener('deviceready', function () {
 	
 // StreamGO()
 
+function playAudio(url) {
+    // Play the audio file at url
+    var my_media = new Media(url,
+        // success callback
+        function () {
+            console.log("playAudio():Audio Success");
+        },
+        // error callback
+        function (err) {
+            console.log("playAudio():Audio Error: " + err);
+        }
+    );
+    // Play audio
+
+}
+//playAudio(StreamGO())
 
 var successCallback = function(result) {  
   console.log('audio callback ' + JSON.stringify(result));  
